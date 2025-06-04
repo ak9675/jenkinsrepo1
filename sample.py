f2f9cb9553059427fa2f1f6a760d0943208000cf
ANSHU KUMAR <98090519+ak9675@users.noreply.github.com>
2025-06-04 15:56:26 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,4 +1,4 @@
-print("hello world from python")
+print"hello world from python")
 print("this is new line added to demo automated gihub commit trigger")
 print("third line for demo")
 print("fourth line for demo")
